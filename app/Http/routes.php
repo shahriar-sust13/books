7492dfe074cd59d81180fa9260ad1eaf512ab522
shahriarsust13 <mk.shahariar@gmail.com>
2016-09-25 03:34:33 +0600

    login page added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
 Route::get('/profile', 'ProfileController@showProfile');
 
 Route::get('/book', 'BookController@showBook');
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -13,8 +13,8 @@
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav btn-lg navbar-right header-menu">
 				@if (Auth::guest())
-					<li class=""><a href="{{ url('/auth/login') }}">Login</a></li>
-					<li><a href="{{ url('/auth/register') }}">Register</a></li>
+					<li class=""><a href="{{ url('/login') }}">Login</a></li>
+					<li><a href="{{ url('/register') }}">Register</a></li>
 				@else
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span</a>
