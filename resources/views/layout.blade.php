fbee2412c7c493ece1fbc21a945a1751267d61db
shahriarsust13 <mk.shahariar@gmail.com>
2016-09-30 03:46:12 +0600

    profile page edited

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,8 +26,7 @@
 	@include('footer')
 
 	<!-- Scripts -->
-	<script src="{{asset('js/common.js')}}"></script>
-	<script src="{{ url( 'js/react/react.js' ) }}"></script>
-	<script src="{{ url( 'js/react/JSXTransformer.js' ) }}"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -16,53 +16,100 @@
 			</div>
 		</div>
 
-		<div class="container book-section">
-			<div class="col-xs-12 section-name-container">
-				<h3>Shared Books</h3>
+		<div class="container profile-body">
+			<div class="profile-nav-section row">
+				<ul class="nav nav-tabs col-md-offset-1 col-md-10">
+				  	<li role="presentation" class="active"><a data-toggle="tab" href="#books">Shared Books</a></li>
+				  	<li role="presentation" class=""><a data-toggle="tab" href="#about">About Me</a></li>
+				</ul>
 			</div>
-			<div class="row">
-				<a href = "#" class="col-md-8 col-md-offset-2 book-container">
-					<div class="col-md-2 book-cover-container">
-						<img src="{{url("images/books/1.jpg")}}" height="138px">
+
+			<div class="tab-content">
+				<div id="books" class="container book-section tab-pane fade in active">
+				<!--
+					<div class="col-xs-12 section-name-container">
+						<h3>Shared Books</h3>
 					</div>
-					<div class="col-md-10 book-info-section">
-						<h3>Python Programming for Dummies</h3>
-						<h4><small>by Maruf Ahmed Mridul</small></h4>
-						<div>
-							<p>
-								This Books is mainly designed for those who know nothing about Python. But if you have basic knowledge of programming then this book will be awesome.
-							</p>
+				-->
+					<div class="row">
+						<a href = "#" class="col-md-8 col-md-offset-2 book-container">
+							<div class="col-md-2 book-cover-container">
+								<img src="{{url("images/books/1.jpg")}}" height="138px">
+							</div>
+							<div class="col-md-10 book-info-section">
+								<h3>Python Programming for Dummies</h3>
+								<h4><small>by Maruf Ahmed Mridul</small></h4>
+								<div>
+									<p>
+										This Books is mainly designed for those who know nothing about Python. But if you have basic knowledge of programming then this book will be awesome.
+									</p>
+								</div>
+							</div>
+						</a>
+						<a href = "#" class="col-md-8 col-md-offset-2 book-container">
+							<div class="col-md-2 book-cover-container">
+								<img src="{{url("images/books/2.jpg")}}" height="138px">
+							</div>
+							<div class="col-md-10 book-info-section">
+								<h3>Computer Architecture</h3>
+								<h4><small>by John Hennessy</small></h4>
+								<div>
+									<p>
+										Computer Architecture (5th Edition) by John Hennessy and David Patterson. Short Description goes here. Short Description goes here.
+									</p>
+								</div>
+							</div>
+						</a>
+						<a href = "#" class="col-md-8 col-md-offset-2 book-container">
+							<div class="col-md-2 book-cover-container">
+								<img src="{{url("images/books/3.jpg")}}" height="138px">
+							</div>
+							<div class="col-md-10 book-info-section">
+								<h3>Introduction to Algorithms</h3>
+								<h4><small>by Thomas Cormen, Leiserson</small></h4>
+								<div>
+									<p>
+										Introduction to Algorithms is a book by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein. It is used as the textbook for algorithms courses at many universities.
+									</p>
+								</div>
+							</div>
+						</a>
+					</div>
+
+					<div class="container pagination-section" style="width: 260px;">
+						<nav aria-label="Page navigation">
+						  	<ul class="pagination">
+						    	<li>
+						    		<a href="#" aria-label="Previous">
+							        	<span aria-hidden="true">&laquo;</span>
+							    	</a>
+							    </li>
+							    <li class="active"><a href="#">1</a></li>
+							    <li><a href="#">2</a></li>
+							    <li><a href="#">3</a></li>
+							    <li><a href="#">4</a></li>
+							    <li><a href="#">5</a></li>
+							    <li>
+								    <a href="#" aria-label="Next">
+								        <span aria-hidden="true">&raquo;</span>
+								    </a>
+							    </li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+
+				<div id="about" class="container tab-pane fade">
+					<div class="container">
+						<div class="info-section col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
+							<h4 class="text-center">Studies at Dept. of Computer Science & Engineering</h4>
+							<h4 class="text-center">Shahjalal University of Science and Technology, Sylhet</h4>
+							<address class="text-center">
+								<strong>Address: </strong> Shahjalal Upashahar, Sylhet	
+							</address>
 						</div>
 					</div>
-				</a>
-				<a href = "#" class="col-md-8 col-md-offset-2 book-container">
-					<div class="col-md-2 book-cover-container">
-						<img src="{{url("images/books/2.jpg")}}" height="138px">
-					</div>
-					<div class="col-md-10 book-info-section">
-						<h3>Computer Architecture</h3>
-						<h4><small>by John Hennessy</small></h4>
-						<div>
-							<p>
-								Computer Architecture (5th Edition) by John Hennessy and David Patterson. Short Description goes here. Short Description goes here.
-							</p>
-						</div>
-					</div>
-				</a>
-				<a href = "#" class="col-md-8 col-md-offset-2 book-container">
-					<div class="col-md-2 book-cover-container">
-						<img src="{{url("images/books/3.jpg")}}" height="138px">
-					</div>
-					<div class="col-md-10 book-info-section">
-						<h3>Introduction to Algorithms</h3>
-						<h4><small>by Thomas Cormen, Leiserson</small></h4>
-						<div>
-							<p>
-								Introduction to Algorithms is a book by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein. It is used as the textbook for algorithms courses at many universities.
-							</p>
-						</div>
-					</div>
-				</a>
+				</div>
 			</div>
 		</div>
 
