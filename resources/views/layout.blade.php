7f176b7f5fe8d9ade45cdbccbe3552fa3bb2301a
shahriarsust13 <mk.shahariar@gmail.com>
2016-10-02 01:57:55 +0600

    layout fit to screen height and notification with jQuery

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,16 +17,27 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-</head>
-<body>
-	@include('header')
-
-	@yield('content')
-
-	@include('footer')
 
 	<!-- Scripts -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+</head>
+<body>
+
+	<div class="main-wrapper">	
+
+		@include('header')
+
+		<div class="body-wrapper">
+			@yield('content')
+		</div>
+
+		<div class="foot-wrapper">
+			@include('footer')
+		</div>
+		
+	</div>
+
 </body>
 </html>
--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -14,7 +14,7 @@
 		<div class="tab-content">
 			<div id="request" class="container request-section tab-pane fade in active">
 				<div class="row">
-					<div class="notification-bar unread-notification col-md-offset-1 col-md-10 col-xs-12">
+					<div class="notification-bar notification-1 unread-notification col-md-offset-1 col-md-10 col-xs-12">
 						<div class="col-md-1 col-xs-2">
 							<img src="{{url("images/request.png")}}"  height="40px">
 						</div>
@@ -26,11 +26,11 @@
 							</h5>
 						</div>
 						<div class="col-md-3 col-md-offset-0 col-xs-offset-6 col-xs-6 notification-btn">
-							<a href="#" class="btn btn-primary btn-success col-md-1">Accept</a>
-							<a href="#" class="btn btn-primary btn-danger col-md-1">Decline</a>
+							<a href="#" class="btn btn-primary btn-success col-md-1 accept-btn-1">Accept</a>
+							<a href="#" class="btn btn-primary btn-danger col-md-1 decline-btn-1">Decline</a>
 						</div>
 					</div>
-					<div class="notification-bar unread-notification col-md-offset-1 col-md-10 col-xs-12">
+					<div class="notification-bar notification-2 unread-notification col-md-offset-1 col-md-10 col-xs-12">
 						<div class="col-md-1 col-xs-2">
 							<img src="{{url("images/request.png")}}"  height="40px">
 						</div>
@@ -42,11 +42,11 @@
 							</h5>
 						</div>
 						<div class="col-md-3 col-md-offset-0 col-xs-offset-6 col-xs-6 notification-btn">
-							<a href="#" class="btn btn-primary btn-success col-md-1">Accept</a>
-							<a href="#" class="btn btn-primary btn-danger col-md-1">Decline</a>
+							<a href="#" class="btn btn-primary btn-success col-md-1 accept-btn-2">Accept</a>
+							<a href="#" class="btn btn-primary btn-danger col-md-1 decline-btn-2">Decline</a>
 						</div>
 					</div>
-					<div class="notification-bar col-md-offset-1 col-md-10 col-xs-12">
+					<div class="notification-bar notification-3 col-md-offset-1 col-md-10 col-xs-12">
 						<div class="col-md-1 col-xs-2">
 							<img src="{{url("images/request.png")}}"  height="40px">
 						</div>
@@ -58,11 +58,11 @@
 							</h5>
 						</div>
 						<div class="col-md-3 col-md-offset-0 col-xs-offset-6 col-xs-6 notification-btn">
-							<a href="#" class="btn btn-primary btn-success col-md-1">Accept</a>
-							<a href="#" class="btn btn-primary btn-danger col-md-1">Decline</a>
+							<a href="#" class="btn btn-primary btn-success col-md-1 accept-btn-3">Accept</a>
+							<a href="#" class="btn btn-primary btn-danger col-md-1 decline-btn-3">Decline</a>
 						</div>
 					</div>
-					<div class="notification-bar col-md-offset-1 col-md-10 col-xs-12">
+					<div class="notification-bar notification-4 col-md-offset-1 col-md-10 col-xs-12">
 						<div class="col-md-1 col-xs-2">
 							<img src="{{url("images/request.png")}}"  height="40px">
 						</div>
@@ -74,11 +74,11 @@
 							</h5>
 						</div>
 						<div class="col-md-3 col-md-offset-0 col-xs-offset-6 col-xs-6 notification-btn">
-							<a href="#" class="btn btn-primary btn-success col-md-1">Accept</a>
-							<a href="#" class="btn btn-primary btn-danger col-md-1">Decline</a>
+							<a href="#" class="btn btn-primary btn-success col-md-1 accept-btn-4">Accept</a>
+							<a href="#" class="btn btn-primary btn-danger col-md-1 decline-btn-4">Decline</a>
 						</div>
 					</div>
-					<div class="notification-bar col-md-offset-1 col-md-10 col-xs-12">
+					<div class="notification-bar notification-5 col-md-offset-1 col-md-10 col-xs-12">
 						<div class="col-md-1 col-xs-2">
 							<img src="{{url("images/request.png")}}"  height="40px">
 						</div>
@@ -90,8 +90,8 @@
 							</h5>
 						</div>
 						<div class="col-md-3 col-md-offset-0 col-xs-offset-6 col-xs-6 notification-btn">
-							<a href="#" class="btn btn-primary btn-success col-md-1">Accept</a>
-							<a href="#" class="btn btn-primary btn-danger col-md-1">Decline</a>
+							<a href="#" class="btn btn-primary btn-success col-md-1 accept-btn-5">Accept</a>
+							<a href="#" class="btn btn-primary btn-danger col-md-1 decline-btn-5">Decline</a>
 						</div>
 					</div>
 				</div>
@@ -164,4 +164,27 @@
 		</div>
 	</div>
 
+<script>
+
+$(document).ready(function(){
+	$(".accept-btn-1").click(function(){
+		$(".notification-1").fadeOut("slow");
+	});
+	$(".accept-btn-2").click(function(){
+		$(".notification-2").fadeOut("slow");
+	});
+	$(".accept-btn-3").click(function(){
+		$(".notification-3").fadeOut("slow");
+	});
+	$(".accept-btn-4").click(function(){
+		$(".notification-4").fadeOut("slow");
+	});
+	$(".accept-btn-5").click(function(){
+		$(".notification-5").fadeOut("slow");
+	});
+});
+
+</script>
+
 @endsection
+
